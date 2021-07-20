2959a790941532712665657a32a65a691fb854f9
moburk <mohammedburk@gmail.com>
2021-07-20 23:23:45 +0530

    Added day 1 exercise 2 json

--- Exercises/Day 1/Ex2.go
+++ Exercises/Day 1/Ex2.go
@@ -1,21 +1,66 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+)
 
-func checkVar (s string) bool {
+type node struct{
+	val byte
+	lNode, rNode *node
+}
+
+func checkOperator (s string) bool {
 	if s == "+" || s == "-" || s == "*" || s == "/" {
 		return true
 	}
 	return false
 }
 
-func preorder(s string) {
-	for i, ch := range s {
+func buildTree (s string) *node{
+	if s == "" {
+		return nil
+	}
+	if len(s) == 1 {
+		if checkOperator(s) {
+			log.Fatalln("Incorrect expression format")
+		}
+		root := node{s[0], nil, nil}
+		return &root
+	}
+	if !checkOperator(string(s[1])) {
+		log.Fatalln("Incorrect expression format")
+	}
+	left := &node{s[0], nil, nil}
+	right := buildTree(s[2:])
+	root := node{s[1], left, right}
+	return &root
+}
 
+func preorder(root *node) {
+	if root != nil {
+		fmt.Printf("%c ", root.val)
+		preorder(root.lNode)
+		preorder(root.rNode)
+	}
+}
+
+func postorder(root *node) {
+	if root != nil {
+		postorder(root.lNode)
+		postorder(root.rNode)
+		fmt.Printf("%c ", root.val)
 	}
 }
 
 func main(){
-	var exp string = "a+b-c"
-	fmt.Println(exp)
+	var exp string
+	fmt.Print("Enter expression: ")
+	fmt.Scan(&exp)
+	var root *node
+	root = buildTree(exp)
+	fmt.Println("Preorder Traversal:")
+	preorder(root)
+	fmt.Println("\nPostorder Traversal:")
+	postorder(root)
 }
