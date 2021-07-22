a1b719a2c42fe0f73273863c47c40c8d92bccd6e
moburk <mohammedburk@gmail.com>
2021-07-22 18:28:06 +0530

    Added Day 2 Exercise 3

--- Exercises/Day 2/Ex2.go
+++ Exercises/Day 2/Ex2.go
@@ -1,9 +1,27 @@
 package main
 
-import "sync"
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+	"sync/atomic"
+	"time"
+)
+
+func addStudentRating(totalRating *int64, wg *sync.WaitGroup) {
+	defer wg.Done()
+	time.Sleep(time.Duration(rand.Intn(100))*time.Millisecond)
+	atomic.AddInt64(totalRating, int64(rand.Intn(6)))
+}
 
 func main() {
 	var wg sync.WaitGroup
-	wg.Add(1)
-	
+	rand.Seed(time.Now().UnixNano())
+	var totalRating int64
+	for i:=0; i<200; i++ {
+		wg.Add(1)
+		go addStudentRating(&totalRating, &wg)
+	}
+	wg.Wait()
+	fmt.Printf("Final rating = %f out of 5\n", float64(totalRating)/200.0)
 }
---
+++ Exercises/Day 2/Ex3.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"sync"
+)
+
+type person struct{
+	name string
+	age int
+}
+
+type bankAccount struct {
+	person
+	mu sync.Mutex
+	accountBalance int
+}
+
+func (account *bankAccount) deposit(money int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	account.mu.Lock()
+	account.accountBalance += money
+	account.mu.Unlock()
+}
+
+func (account *bankAccount) withdraw(money int) error{
+	account.mu.Lock()
+	defer account.mu.Unlock()
+	if account.accountBalance < money {
+		fmt.Println("err")
+		return nil
+		//return errors.New("could not withdraw money due to insufficient balance")
+	}
+	account.accountBalance += money
+	return nil
+}
+
+func main() {
+	var person1 = person{
+		name : "Gopher",
+		age : 22,
+	}
+	var account1 = bankAccount{
+		person: person1,
+		accountBalance: 500,
+	}
+	var wg sync.WaitGroup
+	var amtList = []int{-500, -1000, -700, -300, -500}
+	for _,amt:= range amtList {
+		wg.Add(1)
+		if amt >= 0 {
+			go account1.deposit(amt, &wg)
+		} else {
+			go func() {
+				account1.mu.Lock()
+				defer wg.Done()
+				defer account1.mu.Unlock()
+				if account1.accountBalance < -1*amt {
+					fmt.Println("Error withdrawing", amt, errors.New("could not withdraw money due to insufficient balance"))
+				} else {
+					account1.accountBalance += amt
+				}
+			}()
+		}
+	}
+	wg.Wait()
+	fmt.Println("Final account balance: ", account1.accountBalance)
+}
