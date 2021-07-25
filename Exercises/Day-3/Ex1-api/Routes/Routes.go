74775953f8e09eb432b9236cadec70e555f9efaf
moburk <mohammedburk@gmail.com>
2021-07-26 00:07:28 +0530

    added day 3 reference exercise

---
+++ Exercises/Day-3/Ex1-api/Routes/Routes.go
@@ -0,0 +1,21 @@
+package Routes
+
+import (
+	"Github-Projects/freshers-bootcamp/Exercises/Day-3/Ex1-api/Controllers"
+	"github.com/gin-gonic/gin"
+)
+
+// configuring routes
+
+func SetupRouter() *gin.Engine {
+	r := gin.Default()
+	grp1 := r.Group("/user-api")
+	{
+		grp1.GET("user", Controllers.GetUsers)
+		grp1.POST("user", Controllers.CreateUser)
+		grp1.GET("user/:id", Controllers.GetUserByID)
+		grp1.PUT("user/:id", Controllers.UpdateUser)
+		grp1.DELETE("user/:id", Controllers.DeleteUser)
+	}
+	return r
+}
